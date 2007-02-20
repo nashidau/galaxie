989b2316c28cc77e1d4caba4e415bd16a91ee321
Brett Nash <nash@fst.net>
2007-02-20 18:40:10 +1100

    Start work on messages from boards

--- tpe_board.c
+++ tpe_board.c
@@ -22,8 +22,17 @@ struct board {
 	uint64_t updated;
 };
 
+struct message {
+	int board;
+	int slot;
+	char *title;
+	char *body;
+	int turn;
+};
+
 static int tpe_board_msg_board_list(void *data, int type, void *event);
 static int tpe_board_msg_board_receive(void *data, int type, void *event);
+static int tpe_board_msg_message_receive(void *data, int type, void *event);
 
 struct tpe_board *
 tpe_board_init(struct tpe *tpe){
@@ -37,6 +46,8 @@ tpe_board_init(struct tpe *tpe){
 			tpe_board_msg_board_list, tpe);
 	tpe_event_handler_add(tpe->event, "MsgBoard",
                         tpe_board_msg_board_receive, tpe);
+	tpe_event_handler_add(tpe->event, "MsgMessage",
+                        tpe_board_msg_message_receive, tpe);
 
 
 	return board;
@@ -115,6 +126,7 @@ tpe_board_msg_board_receive(void *data, int type, void *event){
 	char *body;
 	struct board *board;
 	int32_t id;
+	int get[2];
 
 	tpe = data;
 	body = event;
@@ -133,5 +145,39 @@ tpe_board_msg_board_receive(void *data, int type, void *event){
 	printf("Board %d is: %s\n%s\n%d messages\n",id, board->name,
 			board->description, board->nmessages);
 
+	/* Okay - get the messages */
+	get[0] = htonl(id);
+	get[1] = htonl(0); /* purity */
+
+	tpe_msg_send(tpe->msg, "MsgMessageGet", NULL, NULL, get, 4);
+
+	return 1;
+}
+
+static int 
+tpe_board_msg_message_receive(void *data, int type, void *event){
+	struct message *message;
+	struct tpe *tpe;
+	char *body;
+
+	tpe = data;
+	body = event;
+	body += 16;
+
+	message = calloc(1,sizeof(struct message));
+
+	/* FIXME: Doesn't handle reference system */
+	tpe_util_parse_packet(body, "iiassi", &message->board,
+			&message->slot, NULL, NULL,
+			&message->title,
+			&message->body,
+			&message->turn);
+
+	printf("Message is: Board %d Slot %d Turn %d\n"
+		"  Subject: %s\n  Body: %s\n",
+			message->board,message->slot,message->turn,
+			message->title, message->body);
+
+	
 	return 1;
 }
