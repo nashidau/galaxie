21487e57afc12ced11459101632c965a202be4d6
Brett Nash <nash@fst.net>
2007-04-04 09:22:18 +1000

    Update run4.sh script

--- run4.sh
+++ run4.sh
@@ -1,5 +1,5 @@
 
-./tpe --username nash --password password --server localhost >& nash.log &
-./tpai --username badguy --password password --server localhost >& badguy.log &
-./tpai --username elvis --password password --server localhost >& elvis.log&
-./tpai --username smith --password password --server localhost >& smith.log
+./tpe --username nash --password password --server tranquillity.nash.id.au >& nash.log &
+./tpai --username badguy --password password --server tranquillity.nash.id.au >& badguy.log &
+./tpai --username elvis --password password --server tranquillity.nash.id.au >& elvis.log&
+./tpai --username smith --password password --server tranquillity.nash.id.au >& smith.log
