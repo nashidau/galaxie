be69d27bf1a9a2f52c006f27d6e7515f1c04fd3c
Brett Nash <nash@nash.id.au>
2007-04-09 16:15:15 +1000

    Major updates to AI jones

--- ai_jones.c
+++ ai_jones.c
@@ -1,3 +1,14 @@
+/**
+ * Jones is an agressive AI - sepecifically designed to attack other players.
+ *
+ * Assumptions:
+ * 	* Homeworlds are worth 5 battleships
+ *
+ * Algorithm... build 7 battleships
+ * 		Send to attack a homeworld
+ * 		build 1 frigate send to target world
+ *
+ */
 #include <arpa/inet.h>
 #include <assert.h>
 #include <inttypes.h>
@@ -26,6 +37,7 @@ struct ai {
 	struct tpe *tpe;
 };
 
+static int jones_order_fleet(void *data, int type, void *event);
 static int jones_order_planet(void *data, int type, void *event);
 static int jones_order_insert_cb(void *userdata, const char *msgtype,
 		int len, void *edata);
@@ -41,6 +53,8 @@ ai_jones_init(struct tpe *tpe){
 
 	tpe_event_handler_add(tpe->event, "PlanetNoOrders",
 			jones_order_planet, ai);
+	tpe_event_handler_add(tpe->event, "FleetNoOrders",
+			jones_order_fleet, ai);
 
 	return ai;
 }
@@ -72,14 +86,76 @@ jones_order_planet(void *aiv, int type, void *planetv){
 static int 
 jones_order_insert_cb(void *userdata, const char *msgtype,
 		int len, void *edata){
-	
+	struct ai *jones = userdata;
+	int frigate, battleship;
+	int oid,slot,type,turns,nbr,noptions,i,maxstr;
+	struct build_resources *br = NULL;
+	struct arg_type6 *options = NULL;
+	struct object *obj;
+
 	if (strcmp(msgtype, "MsgOrder") != 0){
 		printf("Got a %s back!\n", msgtype);
+		exit(1);
 	}
 
-	printf("Need to handle this\n");
+	tpe_util_parse_packet(edata, "iiiiB6i", 
+			&oid, &slot,&type,&turns,
+			&nbr,&br,
+			&noptions,&options,
+			&maxstr);
 
+	for (i = 0, frigate = 0, battleship = 0 ; i < noptions ; i ++){
+		if (strcmp(options[i].name,"Frigate") == 0){
+			frigate = options[i].id;
+		}
+		if (strcmp(options[i].name,"Battleship") == 0){
+			battleship = options[i].id;
+		}
+	}
+
+	if (frigate == 0 || battleship == 0){
+		printf("Could not find frigate or battleship\n");
+		exit(1);
+	}
+
+	obj = tpe_obj_obj_get_by_id(jones->tpe->obj, oid);
+
+	tpe_orders_object_build(jones->tpe, obj, -1, 
+			"Kill Fleet #9",
+			battleship, 7, 
+			0, 0);
+	tpe_orders_object_build(jones->tpe, obj, -1,
+			"Col Fleet #9",
+			frigate, 1,
+			0, 0);
 	return 1;
 }
 	
+static int
+jones_order_fleet(void *aiv, int type, void *fleetv){
+	struct object *obj = fleetv;
+	struct object_fleet *fleet;
+	struct ai *jones = aiv;
+	int i;
+	const char *designtype;
 
+	fleet = obj->fleet;
+
+	if (fleet == NULL) return 1;
+	for (i = 0 ; i < fleet->nships ; i ++){
+		designtype = tpe_ship_design_name_get(jones->tpe,
+				fleet->ships[i].design);
+		if (designtype == NULL) continue;
+		if (strcmp(designtype, "Frigate") == 0 || 
+				strcmp(designtype,"Battleship") == 0)
+			break;
+	}
+
+	if (strcmp(designtype,"Frigate") == 0){
+
+	} else if (strcmp(designtype,"Battleship") == 0){
+
+	}
+	
+	return 1;
+}
