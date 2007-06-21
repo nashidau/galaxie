3df290decfdd312fff2ff03e8d9550dcda6bd7f2
Brett Nash <nash@nash.id.au>
2007-06-21 16:44:35 +1000

    Basic planet list working.  Next, some actual order entry.

--- gui_list.c
+++ gui_list.c
@@ -1,4 +1,6 @@
 #include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <Ecore_Evas.h>
 #include <Evas.h>
@@ -7,18 +9,59 @@
 #include "tpe_gui.h"
 #include "tpe_gui_private.h"
 #include "gui_window.h"
-
 #include "tpe_obj.h"
+
+enum listtype {
+	LIST_IMAGE,
+	LIST_TEXT,
+};
+
+struct guilist {
+	enum listtype type;
+	const char *header;
+};
+
+
+
+static const struct guilist planetlist[] = {
+	//{ LIST_IMAGE, "Image" },
+	{ LIST_TEXT, "Planet" },
+	{ LIST_TEXT, "Parent" },
+	{ LIST_TEXT, "Owner" },
+};
+
+struct listsource {
+	struct gui *gui;
+	struct guilist *format;
+	Ecore_List *list;
+};	
 
 static unsigned int 
 planet_count_get(void *data){
-	return 10;
+	struct listsource *source = data;
+
+	return ecore_list_nodes(source->list);
 }
 
 
 static void *
 planet_data_fetch(void *data, unsigned int row, unsigned int column){
+	struct listsource *source = data;
+	struct object *o,*p;
 
+	o = ecore_list_goto_index(source->list,row);
+	/* FIXME: Check all args */
+	if (column == 0)
+		return o->name;
+	else if (column == 1){
+		p = tpe_obj_obj_get_by_id(source->gui->tpe, o->parent);
+		if (p)
+			return p->name;
+		else
+			return "Unknown";
+	} else if (column == 2){
+		return "stuff";
+	}
 	return "planet";
 }
 
@@ -26,9 +69,8 @@ static Ewl_Widget *
 planet_widget_fetch(void *data, unsigned int row, unsigned int col){
 	Ewl_Widget *w;
 
-
 	w = ewl_label_new();
-	ewl_label_text_set(EWL_LABEL(w), planet_data_fetch(data,row,col));
+	ewl_label_text_set(EWL_LABEL(w), data);
 
 	ewl_widget_show(w);
 	return w;
@@ -36,33 +78,40 @@ planet_widget_fetch(void *data, unsigned int row, unsigned int col){
 
 static void *
 planet_header_data_fetch(void *data, unsigned int col){
-	if (col == 0) return "Title";
-	else return "Image";
+	struct listsource *source = data;
+
+	return source->format[col].header;
 }
 
 static Ewl_Widget *
-planet_header_widget_fetch(void *data , unsigned int column){
+planet_header_widget_fetch(void *data, unsigned int column){
+	struct listsource *list = data;
 	Ewl_Widget *l;
-
+	
+	/* FIXME: Should sanity check column */
 	l = ewl_label_new();
-	if (column == 0)
-		ewl_label_text_set(EWL_LABEL(l), "Who");
-	else if (column == 1)
-		ewl_label_text_set(EWL_LABEL(l), "Number");
-	else
-		ewl_label_text_set(EWL_LABEL(l), "Email");
+	ewl_label_text_set(EWL_LABEL(l), data);
 	ewl_widget_show(l);
 
 	return l;
 }
 
 void gui_list_planet_add(struct gui *gui){
+	struct listsource *list;
 	Ewl_Widget *win,*wg,*vbox,*tree;
 	Ewl_Model *model;
 	Ewl_View *view;
 	Ecore_List *planets;
 
 	planets = tpe_obj_obj_list_by_type(gui->tpe, OBJTYPE_PLANET);
+
+	list = calloc(1,sizeof(struct listsource));
+	list->gui = gui;
+	list->list = planets;
+	list->format = planetlist;
+	printf("List is %p\n",list);
+
+	/* Most of this needs to be put into a subfunction */
 
 	win = gui_window_ewl_add(gui);
 
@@ -89,7 +138,8 @@ void gui_list_planet_add(struct gui *gui){
         ewl_object_fill_policy_set(EWL_OBJECT(tree), EWL_FLAG_FILL_ALL);
 	ewl_mvc_model_set(EWL_MVC(tree), model);
 	ewl_mvc_view_set(EWL_MVC(tree), view);
-	ewl_tree2_column_count_set(EWL_TREE2(tree), 3);
+	ewl_tree2_column_count_set(EWL_TREE2(tree), 3); /* FIXME */
+	ewl_mvc_data_set(EWL_MVC(tree), list); /* FIXME */
 	ewl_widget_show(tree);
 
 	ewl_widget_show(win);
