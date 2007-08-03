0e78a0465a9f1f9953265bccedf3d9ca9d4b9471
Brett Nash <nash@nash.id.au>
2007-08-04 00:06:27 +1000

    Update run8.sh for a more modern filename.

--- run8.sh
+++ run8.sh
@@ -1,9 +1,9 @@
 
-./tpe --username nash --password password --server localhost >& nash.log &
-./tpai --username badguy --password password --server localhost >& badguy.log &
-./tpai --username elvis --password password --server localhost >& elvis.log &
-./tpai --username smith --password password --server localhost >& smith.log &
-./tpai --username jones --password password --server localhost >& jones.log &
-./tpai --username john --password password --server localhost >& john.log&
-./tpai --username joe --password password --server localhost >& joe.log&
-./tpai --username evil --password password --server localhost >& evil.log
+./galaxie --username nash --password password --server localhost >& nash.log &
+./galaxie --username badguy --password password --server localhost >& badguy.log &
+./galaxie --username elvis --password password --server localhost >& elvis.log &
+./galaxie --username smith --password password --server localhost >& smith.log &
+./galaxie --username jones --password password --server localhost >& jones.log &
+./galaxie --username john --password password --server localhost >& john.log&
+./galaxie --username joe --password password --server localhost >& joe.log&
+./galaxie --username evil --password password --server localhost >& evil.log
