41b6b45f6b7510600a304537ff86613f5723693c
Brett Nash <nash@nash.id.au>
2007-11-09 18:04:45 +1100

    Basic working name and icon.

--- ewl/gewl_object.c
+++ ewl/gewl_object.c
@@ -1,50 +1,85 @@
 #include <assert.h>
 #include <inttypes.h>
+#include <stdlib.h>
 
 #include <Ecore_Data.h>
 #include <Ecore_Evas.h>
 #include <Ewl.h>
 
+struct gui;
 #include "../tpe_obj.h"
 #include "../gui_window.h"
+#include "gewl_object.h"
 
-struct gui;
-
-#define ETK_DATA_MAGIC	"getkdata"
-struct etk_data {
+#define EWL_DATA_MAGIC	"getkdata"
+struct ewl_planet_data {
+	//Evas_Object *window;	
+	Ewl_Widget *window;
 	
+	/* Various pieces */ 
+	Ewl_Widget *name;
+	Ewl_Widget *icon;
+	Ewl_Widget *children;
+	Ewl_Widget *resources;
 };
 
-static void
-window_pos_get(void *data, int *x, int *y){
-	ecore_evas_geometry_get(data, x,y, NULL,NULL);
-}
+void tpe_ewl_planet_set(struct ewl_planet_data *p, struct object *planet);
+
 
 Evas_Object *
 tpe_ewl_planet_add(struct gui *gui, struct object *planet){
+	struct ewl_planet_data *p;
 	Ewl_Widget *window;
-	Ewl_Widget *w;
 	Ewl_Widget *box;
-	Evas *e;
+	
+	p = calloc(1,sizeof(struct ewl_planet_data));
+
 
 	/* FIXME: Move this to a more useful place */
-	window = gui_window_ewl_add(gui);
+	p->window = window = gui_window_ewl_add(gui);
 
 	box = ewl_hbox_new();
 	ewl_container_child_append(EWL_CONTAINER(window), box);
 	ewl_widget_show(box);
-	
-	w = ewl_label_new();
-	ewl_label_text_set(w,planet->name);
-	ewl_container_child_append(EWL_CONTAINER(box), w);
-	ewl_widget_show(w);
+
+	p->name = ewl_label_new();
+	ewl_container_child_append(EWL_CONTAINER(box), p->name);
+	ewl_widget_show(p->name);
+
+	p->icon = ewl_icon_simple_new();
+	ewl_container_child_append(EWL_CONTAINER(box), p->icon);
+	ewl_widget_show(p->icon);
 
 
+	tpe_ewl_planet_set(p, planet);
 
-	/* FIXME */	
+	ewl_widget_show(box);
 	return NULL;	
 }
 
+void
+tpe_ewl_planet_set(struct ewl_planet_data *p, struct object *planet){
+	const char *file;
+
+	assert(p);
+	assert(planet);
+
+	assert(p->window);
+	assert(p->name); assert(p->icon);
+
+	ewl_label_text_set(EWL_LABEL(p->name), planet->name);
+
+	/* Hack */
+	if (planet->type == OBJTYPE_FLEET) file = "edje/images/fleet.png";
+	else if(planet->type == OBJTYPE_GALAXY) file="edje/images/galaxy64.png";
+	else if(planet->type==OBJTYPE_UNIVERSE)file="edje/images/universe.png";
+	else if(planet->type==OBJTYPE_PLANET)file="edje/images/planet.png";
+	else if(planet->type==OBJTYPE_SYSTEM)file="edje/images/star.png";
+	ewl_icon_image_set(EWL_ICON(p->icon), file, NULL);
+
+	/* Resources */
+
+}
 
 
 
